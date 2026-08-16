f32404d5c6add2b8a78e9c2a00f8202e1354fbbc
Jesus <archerblue41@gmail.com>
2026-08-16 14:40:40 +0800

    cleanup

--- src/lib/monaco-loader.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import loader from "@monaco-editor/loader";
-
-if (typeof window !== "undefined") {
-  // This initializes the loader and points it to where the worker files are served.
-  // The webpack plugin in next.config.js handles copying the files.
-  loader.init();
-}
